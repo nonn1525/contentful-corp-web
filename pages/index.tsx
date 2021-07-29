252484fb980140163e0399428579c654faa6299c
Chisato Kato <i.like.nuko@gmail.com>
2021-07-29 12:45:34 +0900

    first-commit

---
+++ pages/index.tsx
@@ -0,0 +1,73 @@
+import React, { VFC } from "react";
+import Head from "next/head";
+import styled from "styled-components";
+import { createClient } from "contentful";
+import { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
+import Link from "next/link";
+
+export const getStaticProps = async () => {
+  const client = createClient({
+    space: process.env.CTF_SPACE_ID || "",
+    accessToken: process.env.CTF_CDA_ACCESS_TOKEN || "",
+  });
+
+  const response: any = await client.getEntries({
+    content_type: "blogPost",
+  });
+
+  return {
+    props: {
+      posts: response.items,
+    },
+  };
+};
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+const Home: NextPage<Props> = ({ posts }) => {
+  return (
+    <div>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+      <PostListStyled>
+        <h3>記事一覧</h3>
+        <ul>
+          {posts.map((item: any) => (
+            <li key={item.sys.id}>
+              <Link href={"/posts/" + item.sys.id}>
+                <a>{item.fields.title}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </PostListStyled>
+    </div>
+  );
+};
+
+export default Home;
+
+const PostListStyled = styled.div`
+  h3 {
+    text-align: center;
+  }
+  ul,
+  ol {
+    padding: 0;
+    position: relative;
+  }
+
+  ul li,
+  ol li {
+    margin: 0 auto;
+    color: #fffffe;
+    border-left: solid 6px #fffffe; /*左側の線*/
+    background: #a7a9be; /*背景色*/
+    margin-bottom: 3px; /*下のバーとの余白*/
+    line-height: 1.5;
+    padding: 0.5em;
+    list-style-type: none !important; /*ポチ消す*/
+    width: 400px;
+  }
+`;
---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,83 @@
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { createClient } from "contentful";
+import {
+  GetStaticPaths,
+  GetStaticProps,
+  InferGetStaticPropsType,
+  NextPage,
+} from "next";
+import { BLOCKS } from "@contentful/rich-text-types";
+import styled from "styled-components";
+
+const client = createClient({
+  space: process.env.CTF_SPACE_ID || "",
+  accessToken: process.env.CTF_CDA_ACCESS_TOKEN || "",
+});
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const res: any = await client.getEntries({
+    content_type: "blogPost",
+  });
+
+  const paths = res.items.map((item: any) => {
+    return {
+      params: { slug: item.sys.id },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }: { params: any }) => {
+  const id = params.slug;
+  const response = await client.getEntry(id);
+
+  return {
+    props: {
+      posts: response,
+    },
+  };
+};
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+const Posts: NextPage<Props> = ({ posts }: { posts: any }) => {
+  const options = {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: (node: any, children: any) => {
+        const src = "https:" + node.data.target.fields.file.url;
+        const height = node.data.target.fields.file.details.height;
+        const width = node.data.target.fields.file.details.width;
+        return <img src={src} width={width} height={height} />;
+      },
+    },
+  };
+  console.log(posts);
+  return (
+    <SlugStyled>
+      <div className="container">
+        <h1>{posts.fields.title}</h1>
+        {posts.fields.body.content.map((item: any) => (
+          <div>{documentToReactComponents(item, options)}</div>
+        ))}
+      </div>
+    </SlugStyled>
+  );
+};
+
+export default Posts;
+
+const SlugStyled = styled.div`
+  .container {
+    margin: 0 20%;
+  }
+
+  ul,
+  ol {
+    border-top: solid #fffffe 1px; /*上のボーダー*/
+    border-bottom: solid #fffffe 1px; /*下のボーダー*/
+    padding: 0.5em 0 0.5em 1.5em;
+  }
+`;
