43f29d1fcf8686844b57e0e3a2b44fb378f265a4
Chisato Kato <i.like.nuko@gmail.com>
2021-07-30 20:06:42 +0900

    Update contentful.ts

--- config/contentful.ts
+++ config/contentful.ts
@@ -1,8 +1,8 @@
 const contentful = require("contentful");
 
 const config = {
-  space: process.env.CTF_SPACE_ID,
-  accessToken: process.env.CTF_CDA_ACCESS_TOKEN,
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
 };
 
 export const createClient = () => {
